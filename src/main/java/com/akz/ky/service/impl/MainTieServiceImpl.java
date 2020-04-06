d5c477e5a4f6f2f1f2e04674ca3c42ba9a6660d9
zkr_lizhixin <972619248@qq.com>
2020-04-06 16:17:12 +0800

    1001-漏发布文件

--- src/main/java/com/akz/ky/service/impl/MainTieServiceImpl.java
+++ src/main/java/com/akz/ky/service/impl/MainTieServiceImpl.java
@@ -1,4 +1,48 @@
 package com.akz.ky.service.impl;
 
-public class MainTieServiceImpl {
+import com.akz.ky.mapper.MainTieMapper;
+import com.akz.ky.pojo.MainTiePojo;
+import com.akz.ky.service.MainTieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author lzx
+ * @version 1.0
+ * @date 2020/3/30 21:28
+ * @Description
+ */
+@Service
+@CacheConfig(cacheNames = "mainTies")
+public class MainTieServiceImpl  implements MainTieService {
+    @Autowired(required = false)
+    MainTieMapper mainTieMapper;
+    @Override
+    @CacheEvict(allEntries = true)
+    public boolean add(MainTiePojo mainTiePojo) {
+        return mainTieMapper.add(mainTiePojo);
+    }
+
+    @Override
+    @Cacheable(key="'mainTies-all'")
+    public List<MainTiePojo> getMainTie() {
+        return mainTieMapper.getMainTie();
+    }
+
+    @Override
+    @CacheEvict(allEntries = true)
+    public boolean update(MainTiePojo mainTiePojo) {
+        return mainTieMapper.update(mainTiePojo);
+    }
+
+    @Override
+    @Cacheable(key="'mainTies-one-'+ #p0")
+    public MainTiePojo getOne(int mainTieNo) {
+        return mainTieMapper.getOne(mainTieNo);
+    }
 }
